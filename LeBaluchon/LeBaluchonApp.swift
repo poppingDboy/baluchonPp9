d6de3a5fc5c14be66ba2f9dc0b1921c70f20329f
Dylan Marteau <dylan_marteau@connect-tech.sncf>
2023-08-29 16:58:20 +0200

    Initial Commit

---
+++ LeBaluchon/LeBaluchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeBaluchonApp.swift
+//  LeBaluchon
+//
+//  Created by MARTEAU Dylan on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeBaluchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
