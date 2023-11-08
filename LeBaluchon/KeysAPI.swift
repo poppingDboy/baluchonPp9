4215bd4e52d5c3bae6908fee90aef17afa70014b
Dylan <109434969+poppingDboy@users.noreply.github.com>
2023-11-08 19:45:27 +0100

    Update KeysAPI.swift

--- LeBaluchon/KeysAPI.swift
+++ LeBaluchon/KeysAPI.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum KeysAPI: String {
-    case currencyKey = "47d6f835b3ad854723c613c2524f0ba2"
-    case traductionKey = "DeepL-Auth-Key 3ccb06bb-2435-35ae-100d-dd2ad5d6c449:fx"
-    case weatherKey = "d5138b4e61ad1fcee107da7b080bd48f"
+    case currencyKey = ".."
+    case traductionKey = ".."
+    case weatherKey = ".."
 }
 
